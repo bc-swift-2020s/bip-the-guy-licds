cdfbed52eb6af3515257717421d3472734577c16
Yi Li <licds@bc.edu>
2020-02-10 13:21:46 -0500

    Completed 4.5

--- bigTheGuy/ViewController.swift
+++ bigTheGuy/ViewController.swift
@@ -7,10 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     
 //MARK PROPERTIES
+    var audioPlayer = AVAudioPlayer()
+    var imagePicker = UIImagePickerController()
+    
     @IBOutlet weak var imageToPunch: UIImageView!
     
     override func viewDidLoad() {
@@ -26,14 +31,61 @@ class ViewController: UIViewController {
         UIView.animate(withDuration: 0.25,delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: {self.imageToPunch.bounds = bounds}, completion: nil)
     }
     
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
+        if let sound = NSDataAsset(name: soundName) {
+            do{
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("ERROR: \(error.localizedDescription) Could not initialize AVAudioPlayer object")
+            }
+        } else {
+            print("ERROR: file \(soundName) didn't load")
+        }
+    }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        imageToPunch.image = selectedImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
+    }
+            //.InfoKey.originalImage] as! UIImage     }
 //MARK ACTIONS
     @IBAction func libraryPressed(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+        
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
     }
     @IBAction func cameraPressed(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+        
+        imagePicker.sourceType = .camera
+       
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
+        } else {
+            showAlert(title: "Camera not Available", message: "There is no camera on this device")
+        }
     }
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
         animateImage()
+        playSound(soundName: "sound0", audioPlayer: &audioPlayer)
     }
     
 }
